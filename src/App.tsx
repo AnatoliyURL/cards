951cf5e3a6c828a474193674d34526cd0096f2f2
anatolypihulin <anatoliy.pihulin@icloud.com>
2022-03-06 18:05:46 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './main.global.css'
+import {hot} from "react-hot-loader/root";
+import {createStore} from "redux";
+import {Layout} from "./shared/Layout";
+import {rootReducer} from "./store/reducer";
+import {composeWithDevTools} from "redux-devtools-extension";
+import {Provider} from "react-redux";
+import {CardsBlock} from "./shared/CardsBlock";
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+function AppComponent() {
+    return(
+        <Provider store={store} >
+            <Layout>
+                <CardsBlock />
+            </Layout>
+        </Provider>
+    )
+}
+
+export const App = hot(AppComponent)
---
+++ src/server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import ReactDom from 'react-dom/server'
+import {indexTemplate} from "./indexTempale";
+import {App} from "../App";
+
+const app = express()
+
+app.use('/static', express.static('./dist/client'))
+
+app.get('/', (req, res) => {
+    res.send(
+        indexTemplate(ReactDom.renderToString(App()))
+    )
+})
+
+app.listen(3000, () => {
+    console.log('Start server')
+})
